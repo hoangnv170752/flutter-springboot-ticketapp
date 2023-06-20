5de9a15737c02420defea217f4846dbc4e659531
hoangnv170752 <hoang.nv.ral@gmail.com>
2023-06-20 07:44:18 +0700

    add the server

--- client/lib/pages/search_result_page.dart
+++ client/lib/pages/search_result_page.dart
@@ -1,3 +1,4 @@
+import 'package:bus_reservation_udemy/models/bus_schedule.dart';
 import 'package:bus_reservation_udemy/providers/app_data_provider.dart';
 import 'package:bus_reservation_udemy/utils/constants.dart';
 import 'package:flutter/material.dart';
@@ -13,10 +14,10 @@ class SearchResultPage extends StatelessWidget {
     final argList = ModalRoute.of(context)!.settings.arguments as List;
     final BusRoute route = argList[0];
     final String departureDate = argList[1];
-    final provider = Provider.of<AppDataProvider>(context);
-    Provider.of<AppDataProvider>(context).getScheduleByRouteName(
-      route.routeName,
-    );
+    // final provider = Provider.of<AppDataProvider>(context);
+    // Provider.of<AppDataProvider>(context).getScheduleByRouteName(
+    //   route.routeName,
+    // );
     return Scaffold(
       appBar: AppBar(
         title: Text('Search Results'),
@@ -28,18 +29,93 @@ class SearchResultPage extends StatelessWidget {
             'Show results for ${route.cityFrom} to ${route.cityTo} on ${departureDate}',
             style: const TextStyle(fontSize: 20.0),
           ),
-          Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: provider.scheduleList
-                  .map(
-                    (schedule) => ListTile(
-                      title: Text(schedule.bus.busName),
-                      subtitle: Text(schedule.bus.busType),
-                      trailing: Text('$currency${schedule.ticketPrice}'),
-                    ),
-                  )
-                  .toList())
+          Consumer<AppDataProvider>(
+            builder: (context, provider, _) => FutureBuilder<List<BusSchedule>>(
+              future: provider.getScheduleByRouteName(route.routeName),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  final scheduleList = snapshot.data!;
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: scheduleList
+                        .map((schedule) => ScheduleItemView(
+                            schedule: schedule, date: departureDate))
+                        .toList(),
+                  );
+                }
+                if (snapshot.hasError) {
+                  return const Text("Failed to fetch data");
+                }
+                return const Text("Please try again");
+              },
+            ),
+          )
         ],
+      ),
+    );
+  }
+}
+
+class ScheduleItemView extends StatelessWidget {
+  final String date;
+  final BusSchedule schedule;
+
+  ScheduleItemView({Key? key, required this.schedule, required this.date});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ListTile(
+              title: Text(schedule.bus.busName),
+              subtitle: Text(schedule.bus.busType),
+              trailing: Text('$currency${schedule.ticketPrice}'),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    'From ${schedule.busRoute.cityFrom}',
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
+                  ),
+                  Text(
+                    'From ${schedule.busRoute.cityTo}',
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    'Departure Time:  ${schedule.departureTime}',
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
+                  ),
+                  Text(
+                    'Total Seats: ${schedule.bus.totalSeat}',
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
